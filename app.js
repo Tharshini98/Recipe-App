20b96b05a50b648fdb34f4d6ac5d748dd6c4bb09
Tharshini98 <70389909+Tharshini98@users.noreply.github.com>
2025-07-02 18:55:01 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const express = require ("express");
-const mongoose = require ("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+app.use(express.json()); 
 
-app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/recipesdb")
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB connection error.", err));
+const mongoURL = process.env.MONGO_URI;
 
-app.use("/api/recipes", routes);
+mongoose.connect(mongoURL)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
-app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Recipe API!');
+});
+
+app.use('/api/recipes', routes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
