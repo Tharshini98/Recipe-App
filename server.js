03c48beb169b60685e61811f47918a9f7ba85294
Tharshini98 <70389909+Tharshini98@users.noreply.github.com>
2025-07-24 19:40:06 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const path = require('path');
 const recipeRoutes = require('./routes/recipe.routes');
-const errorHandler = require('./middlewares/error.middleware');
 
 dotenv.config();
-connectDB();
-
 const app = express();
+
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/api/recipes', recipeRoutes);
 
+
 app.get('/', (req, res) => {
-  res.send('Recipe API is running');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.error('MongoDB connection error:', err));
